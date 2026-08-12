0db563da61f64876732532088c0dba7b16a26a97
PedroLuizBT <pedroluizbarbosateixeira@gmail.com>
2026-08-11 22:30:33 -0300

    aula 2

---
+++ Aula 1.py
@@ -0,0 +1,3 @@
+print("O homem so falou que o mundo precisa de programadores.")
+print("Que eu devo ser um programador porque vale a pena pro futuro")
+print ("vou ajudar meu país e nao sei que lá, pipipi pópópó")
