44410e2219b9e283db2280956cfc41db1397f698
PedroLuizBT <pedroluizbarbosateixeira@gmail.com>
2026-08-14 02:01:45 -0300

    aula 4

---
+++ EXERCÍCIOS TREINO/ficha de loja.py
@@ -0,0 +1,9 @@
+nome = str(input('Qual o seu nome?'))
+cpf = str(input('Qual o seu CPF?'))
+qtd_produto = int(input('Quantos produtos deseja comprar?'))
+valor_total = float(input('Qual o valor total da compra? R$'))
+cartao_fidelidade = str(input('Você possui o cartão fidelidade? Sim/não:'))
+
+ter_cartao = cartao_fideldade = sim
+
+
